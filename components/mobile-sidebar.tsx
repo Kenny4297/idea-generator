6dbcf3ec8c62369eaf6c0e3632b60149c2fa09fe
Kenny Cordero <geckob4i@gmail.com>
2023-10-07 20:56:58 -0500

    Create responsive Sidebar component with Sheet component from shadcn

---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Menu } from "lucide-react"
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+  
+import Sidebar from './sidebar';
+
+
+const MobileSidebar = () => {
+  return (
+        <Sheet>
+            <SheetTrigger asChild>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
+  )
+}
+
+export default MobileSidebar
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
-import { Button } from './ui/button'
-import { Menu } from "lucide-react"
+import MobileSidebar from './mobile-sidebar'
 import { UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
     <div className="flex items-center p-4">
-        <Button variant="ghost" size="icon" className="md:hidden">
-            <Menu />
-        </Button>
+        <MobileSidebar />
         <div className="flex w-full justify-end">
             <UserButton afterSignOutUrl='/' />
         </div>
