3f05a9991a759eab6029a04f8ddc747a0a7b9498
Kenny Cordero <geckob4i@gmail.com>
2023-10-25 08:43:45 -0500

    Work on favicon

--- app/(landing)/layout.tsx
+++ app/(landing)/layout.tsx
@@ -1,21 +1,14 @@
-import Head from 'next/head';
-
 const LandingLayout = ({
     children
     }: {
         children: React.ReactNode;
     }) => {
         return (
-            <>
-            <Head>
-                <link rel="shortcut icon" href="/idea.ico" />
-            </Head>
             <main className="h-full bg-[#333333] overflow-auto">
                 <div className="mx-auto max-w-screen-xl h-full">
                     {children}
                 </div>
             </main>
-            </>
         )
     }
 
