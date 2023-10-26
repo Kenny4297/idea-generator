5be02634293a5d225e662c7131bf7358811c6bb8
Kenny Cordero <geckob4i@gmail.com>
2023-10-25 19:17:13 -0500

    favicon.ico test

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { ToasterProvider } from '@/components/toaster-provider'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,9 +20,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-      <head>
+      <Head>
         <link rel="icon" href="/favicon.ico" />
-      </head>
+      </Head>
         <body className={inter.className}>
           <ToasterProvider />
           {children}</body>
