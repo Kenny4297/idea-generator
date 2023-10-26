a35e3e119ef63b5b2feb4b62390e7d0935502c11
Kenny Cordero <geckob4i@gmail.com>
2023-10-25 19:08:25 -0500

    Add favicon to root layout page

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
         <body className={inter.className}>
           <ToasterProvider />
           {children}</body>
